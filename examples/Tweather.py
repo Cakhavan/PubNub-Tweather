f59d0866a60fb69dc9e0b8ed42428b5768e60051
Cameron Akhavan <camerona@cameron-akhavan.local>
2018-06-22 11:25:35 -0700

    updates

--- examples/Tweather.py
+++ examples/Tweather.py
@@ -46,8 +46,8 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
 #if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-sensor = sensor_args[sys.argv[1]]
-pin = sys.argv[2]
+sensor = 2302
+pin = 4
 #else:
  #   print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
   #  print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
